b5de6df3a5a0a6ff11efad9031aaa77dbaa83684
rafafau <rafal.sipiatowski@icloud.com>
2025-12-30 09:49:34 +0100

    dodanie opcji maxFailures w konfiguracji Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: '50%',
+  maxFailures: process.env.CI ? 5 : undefined,
   reporter: process.env.CI
     ? [['list'], ['junit', { outputFile: 'test-results/results.xml' }]]
     : [['html'], ['list']],
